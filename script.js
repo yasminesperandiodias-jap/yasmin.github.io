37ea1ced2f9d9dc8431401252fb6744455f11d84
Yaasjaponesa <yasmin.esperandio.dias@escola.pr.gov.br>
2026-03-19 08:48:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+script.js
 
+// Seleciona o botão pelo id
+const botao = document.getElementById("botao");
+
+// Adiciona um evento de clique ao botão
+botao.addEventListener("click", function () {
+    // Mostra uma mensagem no console
+    console.log("Botão clicado!");
+
+    // Mostra um alerta na tela
+    alert("Você clicou no botão!");
+});
